7c341cb523f384e7623480656f0516d9284301bf
PabLouD <pablord@gmail.com>
2013-06-11 10:49:42 -0300

    Update descarga.php

--- files/descarga.php
+++ files/descarga.php
@@ -1,7 +1,7 @@
 <?php
 	//error_reporting(E_ALL);
  	//ini_set("display_errors", 1);
-	include "con.php";
+	include "con2.php";
 	$idarchivo=mataInjection($_GET['a']);
 	$ruta="";
 	$sql="SELECT * from fichero where id = '".$idarchivo."'";
